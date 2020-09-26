290e8fe2ac36801ad228409bb5f8c9f6a6ca4a4f
Fatma Elasouad <fxe920@cs.bham.ac.uk>
2020-09-26 16:25:49 +0100

    updated todo list code along
    added a remove specific item from to do list array

--- JS Arrays/list.js
+++ JS Arrays/list.js
@@ -4,22 +4,47 @@ window.setTimeout(function() {
 // ask user for input
 let input = prompt("What would you like to do?");
 
-// check input type
-
+// check input type while it's not "quit"
 while (input !== "quit") {
 	// handle input 
 	if (input === "list") {
-		console.log(todos);
+		listTodos();
+
 	} else if (input === "new") {
-		// ask for new todo
-		const newTodo = prompt("Enter new todo");
-		// add to todo array
-		todos.push(newTodo);
+		addTodo();
+	} else if (input === "delete"){
+		deleteTodo();
 	}
 	// ask again for new input
 	input = prompt("What would you like to do?");
 }
 
+function listTodos() {
+	console.log('******');
+	todos.forEach(function(todo, idx){
+		console.log(idx + ': ' + todo);
+	});
+	console.log('******');
+}
+
+function addTodo() {
+	// ask for new todo
+	const newTodo = prompt("Enter new todo");
+	// add to todo array
+	todos.push(newTodo);
+}
+
+function deleteTodo() {
+	// ask for index of todo to be deleted
+	const index = prompt("Enter index of the todo to delete");
+	// delete that entry in array
+	todos.splice(index, 1);
+	console.log("deleted todo");
+}
+
+
 console.log("Ok, you quite the app!");
+
+ 
 }, 1000)
 
